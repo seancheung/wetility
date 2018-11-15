0e55d650e234b788e2dbd415ce17b00bdd1734e7
Zhang Xuan <xuan.zhang@xiaobao100.com>
2018-11-15 15:32:25 +0800

    wx update

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const Promise = require('bluebird/js/browser/bluebird')
 const renegeratorRuntime = require('regenerator-runtime/runtime')
 
 function promisify(func, context) {
-  return opts => {
-    new Promise((resolve, reject) => {
-      func.call(context, {
-        ...opts,
-        success: resolve,
-        fail: res => {
-          if (res != null && !(res instanceof Error)) {
-            if (typeof res === 'string') {
-              reject(new Error(res))
-            } else {
-              res = res.errMsg || JSON.stringify(res)
+  return opts =>
+    opts.success || opts.fail || opts.complete
+      ? func.call(context, opts)
+      : new utils.Promise((resolve, reject) => {
+          func.call(context, {
+            ...opts,
+            success: resolve,
+            fail: res => {
+              if (res != null && !(res instanceof Error)) {
+                if (typeof res === 'string') {
+                  reject(new Error(res))
+                } else {
+                  res = res.errMsg || JSON.stringify(res)
+                }
+              }
+              reject(res)
             }
-          }
-          reject(res)
-        }
-      })
-    })
-  }
+          })
+        })
 }
 
-module.exports = {
-  promisify,
-  renegeratorRuntime,
-  Promise
+const $wx = {
+  request: promisify(wx.request)
 }
+
+const utils = {
+  Promise,
+  renegeratorRuntime,
+  promisify,
+  $wx
+}
+
+module.exports = utils
